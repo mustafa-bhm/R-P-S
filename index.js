35404901b8fa576eb22368c80e55be09cb29c6f0
mustafa <mustafa.bhm3@gmail.com>
2022-10-31 12:59:02 -0600

    getcomputerCoiceFunction

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("its working !");
+function getComputerChoice() {
+  let hands = ["rock", "paper", "scissors"];
+  let randomNum = Math.floor(Math.random() * 3);
+  return hands[randomNum];
+}
+
+console.log(getComputerChoice());
