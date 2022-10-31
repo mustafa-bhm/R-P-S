17233394bc6ed5a82fd97bad72220bd5dac1d565
mustafa <mustafa.bhm3@gmail.com>
2022-10-31 13:36:06 -0600

    adding playRound function

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-function getComputerChoice() {
+const computerSelection = function getComputerChoice() {
   let hands = ["rock", "paper", "scissors"];
   let randomNum = Math.floor(Math.random() * 3);
   return hands[randomNum];
-}
+};
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+let playerHand = "scissors";
 
-function playerSelection(input) {
-  let playerInput = input.toLowerCase();
+const playerSelection = function getPlayerSelection() {
+  let playerInput = playerHand.toLowerCase();
   return playerInput;
+};
+
+// console.log(playerSelection("scissors"));
+
+function playRound() {
+  let computer = computerSelection();
+  let player = playerSelection();
+  let result = ``;
+  if (computer === player) {
+    result = `its a tie, try again`;
+  } else if (computer === "rock" && player === "paper") {
+    result = `You win, Paper beates Rock`;
+  } else if (computer === "paper" && player === "rock") {
+    result = `You Lose! Paper beats Rock`;
+  } else if (computer === "scissors" && player === "rock") {
+    result = `You Win! Rock beats Scissors`;
+  } else if (computer === "rock" && player === "scissors") {
+    result = `You Lose, Rock beates Scissors`;
+  } else if (computer === "paper" && player === "scissors") {
+    result = `You Win, Scissors beates Paper `;
+  } else if (computer === "scissors" && player === "paper") {
+    result = `You Lose, Scissors beates Paper `;
+  }
+  console.log("c", computer, "p", player);
+  return result;
 }
 
-console.log(playerSelection("scissors"));
+console.log(playRound());
